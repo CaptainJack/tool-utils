eb92fe6f34f3d52de8acfa445eb2a3f56712dd5e
Alexey Chuprin <a.chuprin@capjack.ru>
2020-04-03 18:01:27 +0300

    Add Startable

---
+++ src/commonMain/kotlin/ru/capjack/tool/utils/Startable.kt
@@ -0,0 +1,28 @@
+package ru.capjack.tool.utils
+
+interface MaybeStartable {
+	fun start(): Stoppable?
+	
+	companion object {
+		val DUMMY = object : MaybeStartable {
+			override fun start(): Stoppable? = null
+		}
+	}
+}
+
+interface Startable : MaybeStartable {
+	override fun start(): Stoppable
+	
+	companion object {
+		
+		val DUMMY = object : Startable {
+			override fun start(): Stoppable = Stoppable.DUMMY
+		}
+		inline operator fun invoke(crossinline block: () -> Stoppable): Startable {
+			return object : Startable {
+				override fun start(): Stoppable = block.invoke()
+			}
+		}
+	}
+
+}
