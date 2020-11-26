bc1363ea950faded3d1bacbc770d627942edf05b
Alexey Chuprin <a.chuprin@capjack.ru>
2020-11-26 18:59:37 +0300

    Added ClockAssistant.clock

--- src/jvmMain/kotlin/ru/capjack/tool/utils/assistant/ClockAssistant.kt
+++ src/jvmMain/kotlin/ru/capjack/tool/utils/assistant/ClockAssistant.kt
@@ -1,8 +1,11 @@
 package ru.capjack.tool.utils.assistant
 
 import ru.capjack.tool.utils.Cancelable
+import java.time.Clock
 import java.time.LocalDateTime
 
 interface ClockAssistant : TemporalAssistant {
+	val clock: Clock
+	
 	fun schedule(point: LocalDateTime, code: () -> Unit): Cancelable
 }
--- src/jvmMain/kotlin/ru/capjack/tool/utils/assistant/ExecutorClockAssistant.kt
+++ src/jvmMain/kotlin/ru/capjack/tool/utils/assistant/ExecutorClockAssistant.kt
@@ -7,7 +7,7 @@ import java.time.temporal.ChronoUnit
 import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.TimeUnit
 
-class ExecutorClockAssistant(executor: ScheduledExecutorService, private val clock: Clock) : ExecutorTemporalAssistant(executor), ClockAssistant {
+class ExecutorClockAssistant(executor: ScheduledExecutorService, override val clock: Clock) : ExecutorTemporalAssistant(executor), ClockAssistant {
 	override fun schedule(point: LocalDateTime, code: () -> Unit): Cancelable {
 		val delay = LocalDateTime.now(clock).until(point, ChronoUnit.NANOS).coerceAtLeast(0)
 		return schedule(delay, TimeUnit.NANOSECONDS, code)
